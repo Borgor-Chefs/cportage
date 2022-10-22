4581f4e3370daf0a021646aff1d90d794deacbfa
bruhulance <liam.major91@gmail.com>
2022-10-22 16:57:14 -0400

    added keycode shorthands

---
+++ atoms/keys.py
@@ -0,0 +1,20 @@
+"""
+this is a shorthand for keys
+"""
+
+#############
+# PAGE KEYS #
+#############
+
+PAGEDOWN = "KEY_NPAGE"
+PAGEUP = "KEY_PPAGE"
+
+##############
+# ARROW KEYS #
+##############
+
+DOWNARROW = "KEY_DOWN"
+UPARROW = "KEY_UP"
+LEFT = "KEY_LEFT"
+RIGHT = "KEY_RIGHT"
+
