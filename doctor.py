fbf6e28096d8850299cd6223c48fd55d2f200108
bruhulance <liam.major91@gmail.com>
2022-10-22 16:59:27 -0400

    implement in the future

---
+++ doctor.py
@@ -0,0 +1,10 @@
+"""
+the purpose of this file is to verify the integrity of the installation of cportage.
+
+this checks
+- installed python modules
+- existence of directories for portage
+- existence of native/installed linux commands
+- files (hashes?)
+"""
+
