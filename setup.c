5cb213584e2a778f4a114e4904c3320cced8498a
bruh <cardboardboxpepe@gmail.com>
2023-05-02 20:25:25 -0400

    started rust port

--- setup.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-/**
-* this file is to create the cportage user that can modify config files
-* - have to call `system()` in order to make groups & make users
-* - might use a bash script
-*/
-
