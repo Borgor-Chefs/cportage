9dcda5f22dd9996a5ba07d36869de26d26cde607
bruh <cardboardboxpepe@gmail.com>
2023-06-06 16:06:41 -0400

    added spawning child windows, window teardown and window title drawing

--- src/gui/display.py
+++ src/gui/display.py
@@ -1,110 +1,155 @@
 """
-the module for the Display object, which is a better wrapper around the ncurses pad object
+the module for the Display object, which is a better wrapper around the ncurses
+pad object
 """
 
-## global imports
+# global imports
 import curses
 
-## local imports
+# local imports
 from . import keys
+from core.data import junk_data
+
 
 class Display:
-	def __init__(self, window, title, data):
-		"""
-		creates a pad object based off of the window object
-		"""
+    def __init__(self, window, title, data, xoffset=0, parent=None):
+        """
+        creates a pad object based off of the window object
+        """
 
-		## from arguments
-		self.window = window
-		self.title = title
-		self.data = data
+        # from arguments
+        self.window = window
+        self.title = title
+        self.data = data
 
-		# getting the dimensions of the parent window (usually from stdscr)
-		(ymax, xmax) = window.getmaxyx()
+        # getting the dimensions of the parent window (usually from stdscr)
+        (ymax, xmax) = window.getmaxyx()
 
-		## internal vars
-		# setting the offset
-		self.ysize = ymax
-		self.xsize = int(xmax / 3)
-		self.yoffset = 0
-		self.xoffset = 0
+        # internal vars
+        # setting the offset
+        self.ysize = ymax
+        self.xsize = int(xmax / 3)
+        self.yoffset = 1
+        self.xoffset = xoffset
 
-		# scrolling functionality
-		self.index = 0
+        # scrolling functionality
+        self.index = 0
 
-		# creating internal objects
-		self.pad = curses.newpad(max(ymax, len(data)), self.xsize)
+        # setting a reference to the parent window object
+        self.parent = parent
 
-		# setting up the pad
-		self.__setup__()
+        # creating internal objects
+        self.pad = curses.newpad(max(ymax, len(data)), self.xsize)
 
-	def __setup__(self):
-		"""
-		this function writes the contents of `self.data` to the `self.pad` object
-		"""
+        # setting up the pad
+        self.__setup__()
 
-		# writing self.data to the pad
-		for i, s in enumerate(self.data):
-			self.pad.addnstr(i, 0, s, self.xsize)
+    def __setup__(self):
+        """
+        this function writes the contents of `self.data` to the `self.pad`
+        object
+        """
 
-		# highlighting the first line
-		self.set_highlight(0)
+        # writing the title atop the pad's render box
+        self.window.addnstr(0, self.xoffset, self.title, self.xsize)
 
-	def set_highlight(self, line):
-		return self.pad.chgat(line, 0, curses.A_REVERSE)
+        # writing self.data to the pad
+        for i, s in enumerate(self.data):
+            self.pad.addnstr(i, 0, s, self.xsize)
 
-	def remove_highlight(self, line):
-		return self.pad.chgat(line, 0, curses.A_NORMAL)
+        # highlighting the first line
+        self.set_highlight(0)
 
-	def handle_input(self, char):
-		"""
-		this function makes an action based off of the provided input
-		"""
+    def set_highlight(self, line):
+        """
+        sets a highlight at a given index
+        """
 
-		match char:
+        return self.pad.chgat(line, 0, curses.A_REVERSE)
 
-            ### scrolling keys
+    def remove_highlight(self, line):
+        """
+        removes the highlight at a given index
+        """
 
-			case keys.ARROWDOWN:
-				(ypad, _) = self.pad.getmaxyx()
-				new = min(self.index + 1, ypad - 1)
-				self.remove_highlight(self.index)
-				self.set_highlight(new)
-				self.index = new
+        return self.pad.chgat(line, 0, curses.A_NORMAL)
 
-			case keys.ARROWUP:
-				new = max(self.index - 1, 0)
-				self.remove_highlight(self.index)
-				self.set_highlight(new)
-				self.index = new
+    def spawn_child(self):
+        """
+        this function creates a new Display as a child from the current
+        index of the current window
+        """
 
-			case keys.PGDOWN:
-				(ypad, _) = self.pad.getmaxyx()
-				new = min(self.index + self.ysize, ypad - 1)
-				self.remove_highlight(self.index)
-				self.set_highlight(new)
-				self.index = new
+        # creating new display
+        d = Display(self.window, "Child", junk_data(150, 30),
+                    xoffset=self.xsize, parent=self)
 
-			case keys.PGUP:
-				new = max(self.index - self.ysize, 0)
-				self.remove_highlight(self.index)
-				self.set_highlight(new)
-				self.index = new
+        # returning the new display
+        return d
 
-			case _:
-				pass
+    def destroy(self):
+        """
+        destroying a window via. clearing it, refreshing, then deleting it
+        """
 
-	def refresh(self):
-		"""
-		refreshes and redraws the pad to the screen
-		"""
-		
-		## calculating the offset from the top
-		scroll_mod = 0
-		if self.index > (self.ysize - 1):
-			scroll_mod = min(self.index - self.ysize + 1, len(self.data) - self.ysize)
-	
-		# refreshing the pad
-		self.pad.refresh(scroll_mod, 0, self.yoffset, self.xoffset, self.ysize - 1, self.xsize)
+        # remove the title
+        self.window.addnstr(0, self.xoffset, " " * self.xsize, self.xsize)
 
+        self.pad.clear()  # clear the pad
+        self.refresh()  # refresh the Display object
+        del self.pad  # delete the pad
+        self.window.refresh()  # refresh the main window
+
+    def handle_input(self, char):
+        """
+        this function makes an action based off of the provided input
+        """
+
+        match char:
+
+            # scrolling keys
+            case keys.ARROWDOWN:
+                (ypad, _) = self.pad.getmaxyx()
+                new = min(self.index + 1, ypad - 1)
+                self.remove_highlight(self.index)
+                self.set_highlight(new)
+                self.index = new
+
+            case keys.ARROWUP:
+                new = max(self.index - 1, 0)
+                self.remove_highlight(self.index)
+                self.set_highlight(new)
+                self.index = new
+
+            case keys.PGDOWN:
+                (ypad, _) = self.pad.getmaxyx()
+                new = min(self.index + self.ysize, ypad - 1)
+                self.remove_highlight(self.index)
+                self.set_highlight(new)
+                self.index = new
+
+            case keys.PGUP:
+                new = max(self.index - self.ysize, 0)
+                self.remove_highlight(self.index)
+                self.set_highlight(new)
+                self.index = new
+
+            case _:
+                pass
+
+    def refresh(self):
+        """
+        refreshes and redraws the pad to the screen
+        """
+
+        # calculating the offset from the top
+        scroll_mod = 0
+        if self.index > (self.ysize - 1 - self.yoffset):
+            scroll_mod = min(self.index - self.ysize + 1,
+                             len(self.data) - self.ysize) + self.yoffset
+
+        # refreshing the pad
+        self.pad.refresh(scroll_mod, 0,
+                         self.yoffset, self.xoffset,
+                         self.ysize - 1, self.xsize + self.xoffset)
 
--- src/main.py
+++ src/main.py
@@ -2,30 +2,48 @@
 the main module for the cportage program
 """
 
-## global imports
+# global imports
 import curses
 
-## local imports
+# local imports
 from gui.display import Display
+from gui import keys
 from core.data import junk_data
 
+
 def main(stdscr):
-    
-    view = Display(stdscr, "Title", junk_data(150, 30))
+
+    root = Display(stdscr, "Title", junk_data(150, 30))
     stdscr.refresh()
+
+    # pointer to the current context (window)
+    view = root
 
     while True:
         view.refresh()
 
         # grabbing input from the user
         key = stdscr.getkey()
-        if key == "q":
-            break
 
-        # letting the display handle the input
-        view.handle_input(key)
+        # handling the input
+        match key:
+            # special cases
+            case "q":
+                break
+            case keys.ARROWRIGHT:
+                view = view.spawn_child()
+            case keys.ARROWLEFT:
+                if view.parent:
+                    tmp = view
+                    view = view.parent
+                    tmp.destroy()
+                    del tmp
+            case _:
+                # letting the display handle the input
+                view.handle_input(key)
 
     return 0
+
 
 # default start
 if __name__ == "__main__":
